e2b8870cd6d916e77763254329fa6a4a55cf0b8c
janitz <janis-d@web.de>
2015-09-29 15:15:36 +0200

    only comments
    things...

--- request.py
+++ request.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
+#need to install http://docs.python-requests.org/en/stable/user/install/
 import sys
 import requests
 
 r = requests.get('http://www.der-postillon.com/search/label/Newsticker')
-liste_raw = r.text.split('+++') 	
-i=0	
+liste_raw = r.text.split('+++')
+i=0
 liste=[]
 for strings in liste_raw:
 	if strings[0:2] == '+ ':
 		strings = strings.replace('&quot;', '"')
 		strings = strings.replace('&#39;', "'")
 		strings = strings.replace('&#8211;', "-")
-		if len(strings) < 162 : #länger als sms is egal
+		if len(strings) < 162 : #longer as sms is unimportant
 			strings = "+++" + strings 
 			liste.append(strings)
 			print(strings)
